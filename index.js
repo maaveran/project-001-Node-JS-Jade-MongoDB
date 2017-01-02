af7b7868bf03c9b0c7e74914eba5986bb26c1fbd
maaveran <stephenbalen.ss@gmail.com>
2017-01-02 11:24:35 +0700

    test

---
+++ index.js
@@ -0,0 +1,34 @@
+var express = require('express'),
+	stylus 	= require('stylus'),
+	nib 	= require('nib');
+
+var app = express()
+function compile(str, path){
+	return stylus(str)
+		.set('filename',path)
+		.use(nib())
+}
+
+app.set('views',__dirname + '/views')
+app.set('view engine','jade')
+//app.use(express.logger('dev'))
+app.use(stylus.middleware(
+	{
+		src: __dirname + '/public'
+		, compile :compile
+	}
+))
+app.use(express.static(__dirname  + '/public'))
+app.get('/',(req,res)=>{
+	res.end('hi there')
+})
+app.get('/index',function(req,res){
+	res.render('index',{
+		title : 'Home'
+	})
+})
+app.listen(3000)
+
+
+
+
